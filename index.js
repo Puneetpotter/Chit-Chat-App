ad004bf8ad099bcb7a9f0fa363c70f0c02f4d170
Puneet Rai <73062696+Puneetpotter@users.noreply.github.com>
2022-11-29 10:47:05 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// Node Server to handle socket IO connections.
+const io = require('socket.io')(8000)
+
+const users={};
+
+io.on('connection',socket =>{
+    socket.on('new-user-joined',username=>{
+        console.log("New user", username);
+        users[socket.id]=username;
+        socket.broadcast.emit('user-joined',username);
+    });
+
+    socket.on('send',message => {
+        socket.broadcast.emit('receive',{message: message, username : users[socket.id]})
+    }); 
+})
